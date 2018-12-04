12600dce05b8edaa9bb5fe20c4a3aa7ed9541ebe
Kaur Kaelep <kaur.kaelep@khk.ee>
2018-12-04 14:02:50 +0200

    modified theme `Ignis` version 1.0.5

--- wp-content/themes/ignis/inc/functions/functions-footer.php
+++ wp-content/themes/ignis/inc/functions/functions-footer.php
@@ -23,7 +23,7 @@ add_action('ignis_footer', 'ignis_footer_social_menu', 8);
 function ignis_footer_credits() {
 	?>
 		<div class="site-info">
-			<img href="<?php echo esc_url( __( 'https://i.imgur.com/BNriJVT.jpg', 'ignis' ) ); ?>" rel="nofollow"><?php printf( esc_html__( 'Hei powered by %s', 'ignis' ), 'WordPress' ); ?>
+			<img href="<?php echo esc_url( __( 'https://i.imgur.com/BNriJVT.jpg', 'ignis' ) ); ?>" rel="nofollow"><?php printf( esc_html__( 'Aitäh, et külastad mu lehte!', 'ignis' ), 'WordPress' ); ?>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %2$s by %1$s.', 'ignis' ), 'aThemes', '<a href="https://athemes.com/theme/ignis" rel="nofollow">Ignis</a>' ); ?>
 		</div><!-- .site-info -->
