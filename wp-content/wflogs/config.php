cd21e0f80488b7a12c8244ddd0fb94bc31a20521
Kaur Kaelep <kaur.kaelep@khk.ee>
2018-12-07 13:12:15 +0200

    modified file `config.php` (after deactivation of `Wordfence Security` version 7.1.18)

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1544785807;s:7:"authKey";s:64:"lFPA5U-<^3Nh~X//EXj#I,{fR5X/pI?*w a/PB?<aNZ(SiI1G(h-&)#A}~-oGMd`";}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1544785807;s:7:"authKey";s:64:"lFPA5U-<^3Nh~X//EXj#I,{fR5X/pI?*w a/PB?<aNZ(SiI1G(h-&)#A}~-oGMd`";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1170;}
